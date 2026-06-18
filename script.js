df7a63a6cc0f19e81ca19c611f4190244801a1a3
Nguyễn Bá Quý <150120532+nguyenbaquy1310@users.noreply.github.com>
2026-06-18 11:05:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Script By Nguyen Ba Quy
 console.log(
   "%cBản Quyền thuộc Nguyễn Bá Quý",
-  "color: red; font-size: 16px; font-weight: bold;"
+  "color: red; font-size: 40px; font-weight: bold;"
 );
